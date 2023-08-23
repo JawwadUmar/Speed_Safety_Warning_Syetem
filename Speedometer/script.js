c8627a3c7d43e73e91c71549f0f0f17324a24768
Jawwad <fahadmallick99@gmail.com>
2023-08-23 11:22:03 +0530

    called current sped from geolocation api

--- Speedometer/script.js
+++ Speedometer/script.js
@@ -15,6 +15,9 @@ startButton.addEventListener("click", () => {
 function updateSpeed(position) {
     const currentSpeed = position.coords.speed || 0;
     speedDisplay.textContent = `Speed: ${currentSpeed.toFixed(2)} m/s`;
+
+    // Call the exportSpeed function to export the current speed
+    exportSpeed(currentSpeed);
 }
 
 function onError(error) {
@@ -26,3 +29,127 @@ window.addEventListener("beforeunload", () => {
         navigator.geolocation.clearWatch(watchId);
     }
 });
+
+async function exportSpeed(speed) {
+    // For demonstration purposes, let's simulate an asynchronous delay and log the speed.
+    await new Promise(resolve => setTimeout(resolve, 1)); // Simulating delay
+    console.log(`Exported speed: ${speed}`);
+}
+
+// export { exportSpeed };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const startButton = document.getElementById("startButton");
+// const speedDisplay = document.getElementById("speedDisplay");
+
+// let watchId;
+
+// startButton.addEventListener("click", () => {
+//     if ("geolocation" in navigator) {
+//         startButton.disabled = true;
+//         watchId = navigator.geolocation.watchPosition(updateSpeed, onError, { enableHighAccuracy: true });
+//     } else {
+//         alert("Geolocation is not supported by your browser.");
+//     }
+// });
+
+// function updateSpeed(position) {
+//     const currentSpeed = position.coords.speed || 0;
+//     speedDisplay.textContent = `Speed: ${currentSpeed.toFixed(2)} m/s`;
+
+//     // Call the exportSpeed function to export the current speed
+//     exportSpeed(currentSpeed);
+// }
+
+// function onError(error) {
+//     alert(`Error getting location: ${error.message}`);
+// }
+
+// window.addEventListener("beforeunload", () => {
+//     if (watchId) {
+//         navigator.geolocation.clearWatch(watchId);
+//     }
+// });
+
+// export async function exportSpeed(speed) {
+//     // For demonstration purposes, let's simulate an asynchronous delay and log the speed.
+//     await new Promise(resolve => setTimeout(resolve, 1)); // Simulating delay
+//     console.log(`Exported speed: ${speed}`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const startButton = document.getElementById("startButton");
+// // const speedDisplay = document.getElementById("speedDisplay");
+
+// // let watchId;
+
+// // startButton.addEventListener("click", () => {
+// //     if ("geolocation" in navigator) {
+// //         startButton.disabled = true;
+// //         watchId = navigator.geolocation.watchPosition(updateSpeed, onError, { enableHighAccuracy: true });
+// //     } else {
+// //         alert("Geolocation is not supported by your browser.");
+// //     }
+// // });
+
+// // function updateSpeed(position) {
+// //     const currentSpeed = position.coords.speed || 0;
+// //     speedDisplay.textContent = `Speed: ${currentSpeed.toFixed(2)} m/s`;
+// // }
+
+// // function onError(error) {
+// //     alert(`Error getting location: ${error.message}`);
+// // }
+
+// // window.addEventListener("beforeunload", () => {
+// //     if (watchId) {
+// //         navigator.geolocation.clearWatch(watchId);
+// //     }
+// // });
+
+// // export async function exportSpeed(speed) {
+// //     // For demonstration purposes, let's simulate an asynchronous delay and log the speed.
+// //     await new Promise(resolve => setTimeout(resolve, 1)); // Simulating delay
+// //     console.log(`Exported speed: ${speed}`);
+// // }
+
